91a781ecd155520058fe5720a9cc35387e4e90a4
唐江华 <379170882@qq.com>
2020-07-23 20:50:45 +0800

    init

---
+++ src/class09/Code05_IPO.java
@@ -0,0 +1,64 @@
+package class09;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * 贪心算法的解题套路实战
+ * 输入正数数组costs、正数数组profits、正数K、正数M
+ * costs[i]表示i号项目的花费
+ * 正数数组profits[i］表示i号项目在扣除花费之后还能挣到的钱（利润）
+ * K表示你只能串行的最多做k个项目
+ * M表示你初始的资金 说明
+ * 每做完一个项目 马上获得的收益 可以支持你去做下一个项目。不能并行的做项目
+ * 愉出你最后获得的纽大钱数。
+ */
+public class Code05_IPO {
+
+	public static int findMaximizedCapital(int K, int W, int[] Profits, int[] Capital) {
+		PriorityQueue<Program> minCostQ = new PriorityQueue<>(new MinCostComparator());
+		PriorityQueue<Program> maxProfitQ = new PriorityQueue<>(new MaxProfitComparator());
+		for (int i = 0; i < Profits.length; i++) {
+			minCostQ.add(new Program(Profits[i], Capital[i]));
+		}
+		for (int i = 0; i < K; i++) {
+			while (!minCostQ.isEmpty() && minCostQ.peek().c <= W) {
+				maxProfitQ.add(minCostQ.poll());
+			}
+			if (maxProfitQ.isEmpty()) {
+				return W;
+			}
+			W += maxProfitQ.poll().p;
+		}
+		return W;
+	}
+
+	public static class Program {
+		public int p;
+		public int c;
+
+		public Program(int p, int c) {
+			this.p = p;
+			this.c = c;
+		}
+	}
+
+	public static class MinCostComparator implements Comparator<Program> {
+
+		@Override
+		public int compare(Program o1, Program o2) {
+			return o1.c - o2.c;
+		}
+
+	}
+
+	public static class MaxProfitComparator implements Comparator<Program> {
+
+		@Override
+		public int compare(Program o1, Program o2) {
+			return o2.p - o1.p;
+		}
+
+	}
+
+}
